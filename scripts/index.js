5105868691e288fe01cbe0d952c5c3e9daacf386
Mike Zyatkov <mikhail.zyatkov@essensys.tech>
2021-11-26 17:04:48 +0000

    feat: template tags

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,7 @@ const TODO_LIST = [
 
 
 const listContainerEl = document.querySelector('.todo__list');
+const templateEl = document.querySelector('.template');
 
 function render() {
     const html = TODO_LIST
@@ -18,42 +19,12 @@ function render() {
     listContainerEl.append(...html);
 }
 
-
-function getItemHTML(item) {
-    return `<li class="todo__item card">
-                    <h2 class="card__title">${item.title}</h2>
-                    <div class="card__actions">
-                        <button type="button" class="button button_edit"></button>
-                        <button type="button" class="button button_duplicate"></button>
-                        <button type="button" class="button button_remove"></button>
-                    </div>
-                </li>`
-}
-
 function getItem(item) {
-    const cardItem = document.createElement('li');
-    cardItem.classList.add('todo__item', 'card');
+    const newItem = templateEl.content.cloneNode(true);
+    const headerEl = newItem.querySelector('.card__title');
+    headerEl.textContent = item.title;
 
-    const cardHeader = document.createElement('h2');
-    cardHeader.classList.add('card__title');
-    cardHeader.textContent = item.title;
-
-    const cardActions = document.createElement('div');
-    cardActions.classList.add('card__actions');
-
-    const editButton = document.createElement('button');
-    editButton.classList.add('button', 'button_edit');
-
-    const duplicateButton = document.createElement('button');
-    duplicateButton.classList.add('button', 'button_duplicate');
-
-    const removeButton = document.createElement('button');
-    removeButton.classList.add('button', 'button_remove');
-
-    cardActions.append(editButton, duplicateButton, removeButton);
-    cardItem.append(cardHeader, cardActions);
-
-    return cardItem;
+    return newItem;
 }
 
 render();
