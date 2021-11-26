23418dbf40e4ce175d1f62504642b3472c526fef
Mike Zyatkov <mikhail.zyatkov@essensys.tech>
2021-11-26 17:14:49 +0000

    handle add

--- scripts/index.js
+++ scripts/index.js
@@ -10,6 +10,9 @@ const TODO_LIST = [
 const listContainerEl = document.querySelector('.todo__list');
 const templateEl = document.querySelector('.template');
 
+const addButtonEl = document.querySelector('.button_add');
+const inputEl = document.querySelector('.input');
+
 function render() {
     const html = TODO_LIST
         .map((item, idx, arr) => {
@@ -26,5 +29,15 @@ function getItem(item) {
 
     return newItem;
 }
+
+function handleAdd() {
+    const inputText = inputEl.value;
+    const listItem = getItem({title: inputText});
+    listContainerEl.prepend(listItem);
+
+    inputEl.value = '';
+}
+
+addButtonEl.addEventListener('click', handleAdd);
 
 render();
