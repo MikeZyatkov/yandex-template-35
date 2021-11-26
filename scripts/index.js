950eb800d6a224f66b09909c33826f6a859fb4d5
Mike Zyatkov <mikhail.zyatkov@essensys.tech>
2021-11-26 16:31:13 +0000

    feat: dynamic list rendering

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,30 @@ const TODO_LIST = [
     { title: 'Сделать свой сайт 👨‍💻' },
     { title: 'Погулять в парке 🌳' },
 ];
+
+
+const listContainerEl = document.querySelector('.todo__list');
+
+function render() {
+    const html = TODO_LIST
+        .map((item, idx, arr) => {
+            return getItemHTML(item);
+        })
+        .join('');
+
+    listContainerEl.insertAdjacentHTML('afterbegin', html);
+}
+
+
+function getItemHTML(item) {
+    return `<li class="todo__item card">
+                    <h2 class="card__title">${item.title}</h2>
+                    <div class="card__actions">
+                        <button type="button" class="button button_edit"></button>
+                        <button type="button" class="button button_duplicate"></button>
+                        <button type="button" class="button button_remove"></button>
+                    </div>
+                </li>`
+}
+
+render();
